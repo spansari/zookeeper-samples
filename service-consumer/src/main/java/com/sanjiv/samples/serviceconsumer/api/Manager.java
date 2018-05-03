5f86cac6390222363a50c00eb0fad003e476d9d2
Sanjiv Pansari <sanjiv.pansari@macys.com>
2018-05-03 14:25:06 -0400

    defect fix

--- service-consumer/src/main/java/com/sanjiv/samples/serviceconsumer/api/Manager.java
+++ service-consumer/src/main/java/com/sanjiv/samples/serviceconsumer/api/Manager.java
@@ -4,8 +4,10 @@ import java.io.Serializable;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -14,42 +16,49 @@ import org.apache.curator.x.discovery.ServiceDiscovery;
 import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
 import org.apache.curator.x.discovery.ServiceInstance;
 import org.apache.curator.x.discovery.ServiceProvider;
-import org.apache.curator.x.discovery.strategies.RandomStrategy;
 
 @Path("/api")
 public class Manager implements Serializable {
-	
-	@GET
-    @Path("/delegate")
-    public String delegate() {
-		
-		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181", new RetryNTimes(5, 1000));
-        curatorFramework.start();
-        
-        String response = "";
-        
+	static ServiceProvider serviceProvider = null;
+	static {
 		try {
-			
-			ServiceDiscovery<Object> serviceDiscovery = ServiceDiscoveryBuilder.builder(Object.class).client(curatorFramework).basePath("service-registry").build();
-			serviceDiscovery.start();
 
-			ServiceProvider serviceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("worker").providerStrategy(new RandomStrategy<Object>()).build();
+			CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
+					new RetryNTimes(5, 1000));
+			curatorFramework.start();
+			ServiceDiscovery<Object> serviceDiscovery = ServiceDiscoveryBuilder.builder(Object.class)
+					.client(curatorFramework).basePath("service-registry").build();
+			serviceDiscovery.start();
+			serviceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("worker").build();
 			serviceProvider.start();
-			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Manager started ");
+	}
+
+	@GET
+	@Path("/delegate")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String delegate() {
+
+		String response = "";
+		try {
+
 			ServiceInstance instance = serviceProvider.getInstance();
 			String address = instance.buildUriSpec();
 			System.out.println(address);
 			Client client = ClientBuilder.newClient();
-			
-			response = client.target(address+ "/api/work").request().get().toString();
-			
-			System.out.println(response);
-	        
+
+			response = client.target(address + "/api/work").request(MediaType.TEXT_PLAIN).get(String.class);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println(response);
+
 		return response;
-    }
-	
+	}
+
 }
